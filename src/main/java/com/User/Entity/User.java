7890e741aa1363616cb30b3f42e3c32f48822681
jirip <jiri.pernik@gmail.com>
2023-10-13 21:22:44 +0200

    entities updated

--- src/main/java/com/User/Entity/User.java
+++ src/main/java/com/User/Entity/User.java
@@ -1,11 +1,15 @@
 package com.User.Entity;
 
+import com.Acknowledgement.Entity.Acknowledgement;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,5 +33,8 @@ public class User {
   @NotEmpty
   @Pattern(regexp = "^(.+)@(\\S+)$")
   private String email;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+  private List<Acknowledgement> acknowledgement;
 
 }
